1b0e7b691003ae5ab2fa0cb2a2481d824a9e6529
Ashot Galstyan <ashotgalstyan@MacBook-Pro-114.local>
2022-04-11 10:32:38 +0400

    finding the biggest member of the array

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,16 @@
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int[] array= new int[] {1,4,5,7,18,2,5};
+        int max = array[0];
+        for (int i=0; i< array.length; i++)
+        {
+            if (array[i]>max)
+            {
+                max= array[i];
+            }
+        }
+
+        System.out.println("max=" + max);
+
     }
 }
